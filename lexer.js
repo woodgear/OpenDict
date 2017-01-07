d4419fb3061eab3cdb109ac41432c7ed1aba5ee8
saber.wu <saber.wu@trantect.com>
2017-01-07 16:49:28 +0800

    refactor

--- lexer.js
+++ lexer.js
@@ -1,3 +1,4 @@
+//TODO str in <a></a>
 function node_token(text_node = null, node_words = []) {
     this.node = text_node;
     this.words = node_words;
@@ -16,52 +17,53 @@ function parseParagraph(root) {
     while (queue.length != 0) {
         let parent = queue.pop();
         for (let i = 0; i < parent.childNodes.length; i++) {
-            let child=parent.childNodes[i];
-            if (child.nodeType==1) {
-              queue.push(child)
-            }else if (child.nodeType==3) {
-              let list=parseWords(child.nodeValue)
-              if (list.length>0) {
-                nodes.push(new node_token(child,list))
-              }
+            let child = parent.childNodes[i];
+            if (child.nodeType == 1) {
+                queue.push(child)
+            } else if (child.nodeType == 3) {
+                let list = parseWords(child.nodeValue)
+                if (list.length > 0) {
+                    nodes.push(new node_token(child, list))
+                }
             }
         }
     }
     return nodes;
 }
 
-    //状态与条件放在一起
-    function parseWords(val) {
-        function isWords(char) {
-            return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || char === '\'';
-        }
-
-        let state = 0;
-        let words = [];
-        let token = {};
-        for (let i = 0; i < val.length; i++) {
-            let char = val[i];
-            if (isWords(char) && state === 1) {
-                token["end"]++;
-                token["val"] += val[i];
-            } else if (isWords(char) && state === 0) {
-                token["start"] = i;
-                token["end"] = i;
-                token["val"] = val[i];
-                state = 1;
-            } else if (!isWords(char) && state === 1) {
-                token["end"]++;
-                if (isNewWord(token["val"])) {
-                    words.push(token);
-                }
-                token = {}
-                state = 0;
-            } else if (!isWords(char) && state === 0) {
-                continue;
-            }
-        }
-        if (state == 1) {
-            words.push(token)
-        }
-        return words;
+//状态与条件放在一起
+function parseWords(val) {
+    function isWords(char) {
+        return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || char === '\'';
     }
+
+    let state = 0;
+    let words = [];
+    let token = {};
+    for (let i = 0; i < val.length; i++) {
+        let char = val[i];
+        if (isWords(char) && state === 1) {
+            token["end"]++;
+            token["val"] += val[i];
+        } else if (isWords(char) && state === 0) {
+            token["start"] = i;
+            token["end"] = i;
+            token["val"] = val[i];
+            state = 1;
+        } else if (!isWords(char) && state === 1) {
+            token["end"]++;
+            if (token["val"].length > 1 && isNewWord(token["val"].toLowerCase())) {
+                words.push(token);
+            }
+            token = {}
+            state = 0;
+        } else if (!isWords(char) && state === 0) {
+            continue;
+        }
+    }
+    if (state == 1&&token["val"].length > 1 && isNewWord(token["val"].toLowerCase())) {
+        token["end"]++;
+        words.push(token)
+    }
+    return words;
+}
--- renderWord.js
+++ renderWord.js
@@ -1,27 +1,31 @@
 function render(ele, tokens) {
-    val = ele.nodeValue
-    //从开始到第一个单词
-    nodes = []
+    let nodes = []
+
     let start = 0;
     let end = 0;
+    let val = ele.nodeValue
+
     for (var i = 0; i < tokens.length; i++) {
         end = tokens[i].start
         let text = document.createTextNode(val.substring(start, end))
-        let p = document.createElement("P");
         nodes.push(text)
-        start = tokens[i].end
+
+        let p = document.createElement("P");
         p.className += "dict"
         text = document.createTextNode(val.substring(tokens[i].start, tokens[i].end))
         p.appendChild(text)
         nodes.push(p)
+
+        start = tokens[i].end
     }
+
     if (tokens[tokens.length - 1].end != val.length) {
         text = document.createTextNode(val.substring(tokens[tokens.length - 1].end, val.length))
         nodes.push(text)
     }
 
-
     let p = document.createElement("P");
+
     for (let e of nodes) {
         p.appendChild(e);
     }
