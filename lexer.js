7e8793fe7545c4cb001f75c6adbfef3ac0b6a1b7
wucong <1875486458@qq.com>
2017-01-07 23:58:05 +0800

    need think about state

--- lexer.js
+++ lexer.js
@@ -31,39 +31,94 @@ function parseParagraph(root) {
     return nodes;
 }
 
-//状态与条件放在一起
-function parseWords(val) {
-    function isWords(char) {
-        return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || char === '\'';
+function isAlpha(str) {
+    return /[A-Za-z]/.test(str.toLowerCase())
+}
+fmap = []
+
+function bind(target, f) {
+    //target {form:"accept",to:"end"} or accept
+    form = target.form;
+    to = target.to
+    if (form && to) {
+        fmap[form][to] = f
+    } else if(form){
+      fmap[form]["*"]=f
+    }else if (to) {
+      fmap["*"][to]=f
+    }
+}
+
+//调用函数的函数名 嗯 也许应该以继承的形式? 每个类默认给trans一个值?
+function trans(to) {
+    return stateMap[to];
+}
+
+stateMap = {
+    start: function(event) {
+        console.log("start");
+        if (/[A-Za-z]/.test(event.val)) {
+            return trans("accept");
+        } else {
+            return trans("start");
+        }
+    },
+    accept: function(event) {
+        console.log("accept");
+        if (/[A-Za-z_\-\']/.test(event.val)) {
+            return trans("accept");
+        } else {
+            return trans("end");
+        }
+    },
+    end: function() {
+        console.log("end");
+        return trans("start")
+    }
+}
+
+function parseWords(str) {
+    state = stateMap.start;
+    for (let i = 0; i < str.length; i++) {
+        state = state({
+            val: str[i]
+        })
     }
 
-    let state = 0;
-    let words = [];
-    let token = {};
-    for (let i = 0; i < val.length; i++) {
-        let char = val[i];
-        if (isWords(char) && state === 1) {
-            token["end"]++;
-            token["val"] += val[i];
-        } else if (isWords(char) && state === 0) {
-            token["start"] = i;
-            token["end"] = i;
-            token["val"] = val[i];
-            state = 1;
-        } else if (!isWords(char) && state === 1) {
-            token["end"]++;
-            if (token["val"].length > 1 && isNewWord(token["val"].toLowerCase())) {
-                words.push(token);
-            }
-            token = {}
-            state = 0;
-        } else if (!isWords(char) && state === 0) {
-            continue;
-        }
-    }
-    if (state == 1&&token["val"].length > 1 && isNewWord(token["val"].toLowerCase())) {
-        token["end"]++;
-        words.push(token)
-    }
-    return words;
+    // function isWords(char) {
+    //     return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || char === '\'';
+    // }
+    //
+    // let state = 0;
+    // let words = [];
+    // let token = {};
+    // for (let i = 0; i < val.length; i++) {
+    //     let char = val[i];
+    //     if (isWords(char) && state === 1) {
+    //         token["end"]++;
+    //         token["val"] += val[i];
+    //     } else if (isWords(char) && state === 0) {
+    //         token["start"] = i;
+    //         token["end"] = i;
+    //         token["val"] = val[i];
+    //         state = 1;
+    //     } else if (!isWords(char) && state === 1) {
+    //         token["end"]++;
+    //         if (token["val"].length > 1 && isNewWord(token["val"].toLowerCase())) {
+    //             words.push(token);
+    //         }
+    //         token = {}
+    //         state = 0;
+    //     } else if (!isWords(char) && state === 0) {
+    //         continue;
+    //     }
+    // }
+    // if (state == 1 && token["val"].length > 1 && isNewWord(token["val"].toLowerCase())) {
+    //     token["end"]++;
+    //     words.push(token)
+    // }
+    // return words;
 }
+
+parseWords("this is a apple")
+console.log("test");
